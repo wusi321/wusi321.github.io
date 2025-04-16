8f49268d2b5816ac6e15450347462fd917520497
wusi321 <19589917063@163.com>
2025-04-17 01:01:53 +0800

    feat: test

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -4,37 +4,40 @@ import appConfig from '@/config.json';
 import { DocSearchModal, useDocSearchKeyboardEvents } from '@docsearch/react';
 import '@docsearch/css';
 import { RootPortal } from './RootPortal';
-import React, { RefObject, useRef } from 'react';
+// 调整后的导入方式
+import { useRef } from'react';
+type RefObject = React.RefObject;
 
 export function SearchPanel() {
-  const [isOpen, setIsOpen] = useAtom(searchPanelOpenAtom);
-  const searchButtonRef: RefObject<HTMLButtonElement> = useRef(null);
+    const [isOpen, setIsOpen] = useAtom(searchPanelOpenAtom);
+    // 创建 searchButtonRef
+    const searchButtonRef: RefObject<HTMLButtonElement> = useRef(null);
 
-  const onOpen = () => {
-    setIsOpen(true);
-  };
-  const onClose = () => {
-    setIsOpen(false);
-  };
+    const onOpen = () => {
+        setIsOpen(true);
+    };
+    const onClose = () => {
+        setIsOpen(false);
+    };
 
-  useDocSearchKeyboardEvents({
-    isOpen,
-    onOpen,
-    onClose,
-    searchButtonRef
-  });
+    useDocSearchKeyboardEvents({
+        isOpen,
+        onOpen,
+        onClose,
+        searchButtonRef
+    });
 
-  return (
-    isOpen && (
-      <RootPortal>
-        <DocSearchModal
-          appId={appConfig.docSearch.appId}
-          apiKey={appConfig.docSearch.apiKey}
-          indexName={appConfig.docSearch.indexName}
-          initialScrollY={window.scrollY}
-          onClose={onClose}
-        />
-      </RootPortal>
-    )
-  );
-}    
+    return (
+        isOpen && (
+            <RootPortal>
+                <DocSearchModal
+                    appId={appConfig.docSearch.appId}
+                    apiKey={appConfig.docSearch.apiKey}
+                    indexName={appConfig.docSearch.indexName}
+                    initialScrollY={window.scrollY}
+                    onClose={onClose}
+                />
+            </RootPortal>
+        )
+    );
+}
